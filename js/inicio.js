89e67824b59625fbf474f7a81f531c01cb77fc5e
Jamesllm <ronaljamesllapapascamontes@gmail.com>
2023-04-03 22:59:32 -0500

    deply -1

--- js/inicio.js
+++ js/inicio.js
@@ -1,5 +1,3 @@
-// importamos los modulos
-import { data } from "./data.js";
 // definimos las constantes
 const video = document.querySelector(".vid");
 const iconPlayPause = document.getElementById("iconPlay");
@@ -11,11 +9,39 @@ const modalImage = document.getElementById("img01");
 const modalFrame = document.getElementById("frame");
 const captionText = document.getElementById("caption");
 const img = document.getElementById("myImage");
+const sideNav = document.querySelector(".sidenav");
+const mainContent = document.querySelector(".main-content");
+
+// data
+const data = [
+  {
+    id: 1,
+    title: "Kimetsu no Yaiba",
+    title2: "Guardianes de la Noche",
+    info: "El joven Tanjiro Kamado vive felizcon su madre y hermanos pequeños en la montaña, subsistiendo humildemente gracias a la venta de carbón. Una fría mañana, después de regresar de trabajar, se encuentraa toda su familia asesinada con la excepción de su hermana Nezuko, convertida en demonio",
+    video: "./videos/demon-slayer.mp4",
+    poster: "./img/banner.jpg",
+  },
+  {
+    id: 2,
+    title: "Violet Evergarden",
+    title2: "",
+    info: 'La historia se centra alrededor de las llamadas "Auto Memory Dolls"; muñecas inicialmente creadas por un científico para asistir a su esposa ciega a escribir sus novelas y, luego, rentarlas a personas que necesitan de sus servicios. Mientras que la función inicial de las Auto Memory Dolls es solo generar las voces a texto, un segundo grupo de personas creó una compañía que renta hermosas y talentosas mujeres quienes, teniendo las mismas funciones que una Auto Memory Doll original, también pueden realizar tareas relacionadas con la milicia o el ejército.',
+    video: "./videos/violet-trailer.mp4",
+    poster: "./img/series/jGaGlwJbg1MqUjswCe3zxTHbo8G.jpg",
+  },
+
+  {
+    id: 3,
+    title: "The Seven Deadly Sins",
+    title2: "La maldición de la luz",
+    info: 'Con la ayuda del "Dragón Sin of Wrath" Meliodas y los peores rebeldes de la historia, los Siete Pecados Capitales, la "Guerra Santa", en la que cuatro razas, incluyendo Humanos, Diosas, Hadas y Gigantes lucharon contra los Demonios, finalmente se sobre. A costa de la vida de Escanor "El pecado del orgullo del león", el Rey Demonio fue derrotado y el mundo recuperó la paz. Después de eso, cada uno de los pecados toma su propio camino.',
+    video: "./videos/the-seven.mp4",
+    poster: "./img/series/vkjsoMF86dJIv6Sgtd4CcuR8kzh.jpg",
+  },
+];
 
 function closeMenu() {
-  let sideNav = document.querySelector(".sidenav");
-  let mainContent = document.querySelector(".main-content");
-
   sideNav.classList.add("side-none");
   if (sideNav.classList.contains("side-none")) {
     mainContent.classList.add("main-full");
